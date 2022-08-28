d1d37061dd378e868544c9a477c70c650bd976a3
JoseSalas98 <josesalasbolivar98@gmail.com>
2022-08-28 17:14:15 -0500

    Se eliminarón archivos redundantes

--- data/core/constans.py
+++
@@ -1,8 +0,0 @@
-#   Importamos las librerias
-import os
-from pathlib import Path
-
-MAIN_DIR = Path(f"{os.path.dirname(os.path.realpath(__file__))}").parent
-CORE_DIR = Path(f"{os.path.dirname(os.path.realpath(__file__))}")
-LOG_DIR = Path(f"{MAIN_DIR}/logs")
-
