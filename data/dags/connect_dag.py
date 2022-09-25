f8c6a5255ead86a6ab26f3d3b9cba2b71af9d9d8
José Salas <josesalasbolivar98@gmail.com>
2022-09-24 21:30:30 -0500

    Se modifica el DAG de conexión

--- data/dags/connect_dag.py
+++ data/dags/connect_dag.py
@@ -21,44 +21,44 @@ month = now.month
 day = now.day
 date = f"{day:02d}-{month:02d}-{year}"
 today_date = date.format(
-                        year = year, 
-                        month = month, 
-                        day = day
-                        )
+    year=year,
+    month=month,
+    day=day
+)
 
 logging.basicConfig(
-                    filename = f"{LOG_DIR}/{today_date}-{db_user}.log", 
-                    level = "DEBUG"
-                    )
+    filename=f"{LOG_DIR}/{today_date}-{db_user}.log",
+    level="DEBUG"
+)
 
 
 # Definimos las propiedades del DAG
 default_args = {
-                "owner": "airflow",
-                "depends_on_past": False,
-                "email": ["airflow@example.com"],
-                "email_on_failure": False,
-                "email_on_retry": False,
-                "retries": 5,
-                "retry_delay": timedelta(seconds = 30),
-                }
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 5,
+    "retry_delay": timedelta(seconds=30),
+}
 
 
 def start_connct(
-                db_name = db_name, 
-                db_user = db_user, 
-                db_pass = db_pass, 
-                db_host = db_host, 
-                db_port = db_port, 
-                schema_name = SCHEMA_NAME
-                ):
+    db_name=db_name,
+    db_user=db_user,
+    db_pass=db_pass,
+    db_host=db_host,
+    db_port=db_port,
+    schema_name=SCHEMA_NAME
+):
     db_string = "postgresql://{}:{}@{}:{}/{}".format(
-                                                    db_user, 
-                                                    db_pass, 
-                                                    db_host, 
-                                                    db_port, 
-                                                    db_name
-                                                    )
+        db_user,
+        db_pass,
+        db_host,
+        db_port,
+        db_name
+    )
     retry_flag = True
     connect_flag = True
     retry_count = 0
@@ -70,7 +70,7 @@ def start_connct(
             connect_flag = False
 
             while retry_flag and retry_count < 5:
-                
+
                 try:
                     engine = create_engine(db_string)
                     engine.connect()
@@ -78,7 +78,8 @@ def start_connct(
                     if insp_db_connect.has_table("uba_kenedy") and insp_db_connect.has_table("lat_sociales_cine"):
                         retry_flag = False
                     else:
-                        logging.info("alguno de los esquemas no existe, reintentando")
+                        logging.info(
+                            "alguno de los esquemas no existe, reintentando")
                         retry_count += 1
                         time.sleep(60)
                 except exc.SQLAlchemyError:
@@ -90,15 +91,16 @@ def start_connct(
             retry_count += 1
     return engine
 
+
 with DAG(
-        "db_start_connect_retry_dag",
-        default_args = default_args,
-        description =  "DAG reintentar establecer la conexión con la base de datos",
-        schedule_interval = timedelta(hours = 1),
-        start_date = datetime(2022, 8, 23),
-        tags = ["alkemy_acceleration_sptr01"],
-        ) as dag:
-        start_connct_task = PythonOperator(
-                                            task_id = "start_connct_task", 
-                                            python_callable = start_connct
-                                        )
+        "connect_dag",
+        default_args=default_args,
+        description="DAG reintentar establecer la conexión con la base de datos",
+        schedule_interval=timedelta(hours=1),
+        start_date=datetime(2022, 8, 23),
+        tags=["alkemy_acceleration_sptr01"],
+) as dag:
+    start_connct_task = PythonOperator(
+        task_id="start_connct_task",
+        python_callable=start_connct
+    )
